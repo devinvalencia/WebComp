2c2dc5f86f02ebbfeec9ac2d1f487fc35bdb3934
devinvalencia <37150528+devinvalencia@users.noreply.github.com>
2021-06-21 01:20:29 -0400

    Updates

--- src/x-425618-project-name1/index.js
+++ src/x-425618-project-name1/index.js
@@ -4,7 +4,9 @@ import styles from './styles.scss';
 
 const view = (state, {updateState}) => {
 	return (
-		<div>Test!</div>
+		<div>https://developer.servicenow.com/connect.do#!/event/knowledge2021/CCL1043-K21/lab_CCL1043-K21_exercise_3_component_hierarchy</div>
+		<div>https://developer.servicenow.com/dev.do#!/reference/now-experience/quebec/ui-framework/main-concepts/component</div>
+		
 	);
 };
 
